c9cb9f372c2fa1eb388aff1eadb4ca3001fe52f7
Exequiel Moneva <exequiel.moneva@intive.com>
2020-07-21 12:38:37 -0300

    test for Heroku

--- classifier/app.py
+++ classifier/app.py
@@ -16,18 +16,7 @@ Routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            image_path = os.path.join('static', uploaded_file.filename)
-            uploaded_file.save(image_path)
-            class_name = inference.get_prediction(image_path)
-            print('CLASS NAME=', class_name)
-            result = {
-                'class_name': class_name,
-                'image_path': image_path
-            }
-            return render_template('show.html', result=result)
-    return render_template('index.html')
+        return "Hello World!"
 
 
 if __name__ == '__main__':
